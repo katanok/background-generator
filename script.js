4a4d6063a23c8cd848094b5724b016dc7b4cd8d1
katanok <105131967+katanok@users.noreply.github.com>
2022-06-06 17:43:25 -0400

    Add files via upload
    Added button to generate a background with two random colors

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var rnd = document.querySelector("button");
 
 body.style.background= "linear-gradient(to right, rgb(83, 234, 203), rgb(237, 90, 232))";
 css.textContent = body.style.background + ";";
@@ -13,5 +14,29 @@ function setGradient() {
   css.textContent = body.style.background + ";";
 }
 
+function randomRGB() {
+  let r = Math.floor(Math.random()*256);
+  let g = Math.floor(Math.random()*256);
+  let b = Math.floor(Math.random()*256);
+
+  return [r, g, b];
+}
+
+function rgbHex() {
+  let [r, g, b] = randomRGB();
+  let rh = r.toString(16).padStart(2, "0");
+  let gh = g.toString(16).padStart(2, "0");
+  let bh = b.toString(16).padStart(2, "0");
+
+  return "#" + rh + gh + bh;
+}
+
+function assignRGB() {
+  color1.value = rgbHex();
+  color2.value = rgbHex();
+  setGradient();
+}
+
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
+rnd.addEventListener("click", assignRGB);
